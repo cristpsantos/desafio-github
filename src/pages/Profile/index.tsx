0e754d752217b02433bc0645ef888551fdb18ba7
cristpsantos <pontescr@gmail.com>
2022-02-07 20:47:56 -0300

    Page profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
 import Button from 'components/Button';
+
 import './styles.css';
 
 const Profile = () => {
@@ -10,6 +11,28 @@ const Profile = () => {
                     <input className="profile-input" type="text" placeholder="digite o perfil"/>
                     <Button description="Encontrar" />
                 </form>
+            </div>
+            <div>
+                <div className="profile-container">
+                    <div className="profile-img">
+                        <img src="https://avatars.githubusercontent.com/u/13897257?v=4" alt="Imagem do perfil"/>
+                    </div>
+                    <div className="profile-info">
+                        <h3>Informações</h3>
+                        <div className="profile-item">
+                            <span>Perfil:</span><p><a href="https://api.github.com/users/acenelio">https://api.github.com/users/acenelio</a></p>
+                        </div>
+                        <div className="profile-item">
+                            <span>Seguidores:</span><p>4379</p>
+                        </div>
+                        <div className="profile-item">
+                            <span>Localidade:</span><p>Uberlândia</p>
+                        </div>
+                        <div className="profile-item">
+                            <span>Nome:</span><p>Nélio Alves</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
